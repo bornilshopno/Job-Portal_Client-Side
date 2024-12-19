994f410e5b3f24491a87ca3a9af491e4df940f44
Ashraf Manna <optimisticashraf@gmail.com>
2024-12-20 03:36:31 +0600

    JWT in onauthchange

--- src/Core/Auth/AuthProvider.jsx
+++ src/Core/Auth/AuthProvider.jsx
@@ -2,39 +2,55 @@ import { useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "./firebase.init";
+import axios from "axios";
 
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setloading] = useState(true);
-    const [jobs,setJobs]=useState([])
+    const [jobs, setJobs] = useState([])
     const createUser = (email, password) => {
         setloading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const loginUser=(email,password)=>{
+    const loginUser = (email, password) => {
         setloading(true);
-        return signInWithEmailAndPassword(auth,email,password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const handleSignOut=()=>{
+    const handleSignOut = () => {
         signOut(auth)
-        .then(res=>console.log("user Signed Out"))
-        .then(error=>console.log(error))
-      }
+            .then(res => console.log("user Signed Out"))
+            .then(error => console.log(error))
+    }
 
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser)
-            setloading(false)
+            if (currentUser?.email) {
+                const user = { email: currentUser.email }
+                axios.post("http://localhost:5000/jwt", user, { withCredentials: true })
+                    .then(res => { console.log("LoggedinUser :", res.data); setloading(false) })
+
+            }
+            else {
+                axios("http://localhost:5000/logout", {}, { withCredentials: true })
+                    .then(res => {
+                        console.log("logout :", res.data);
+                        setloading(false)
+                    })
+            }
+
+
+
         })
-        return ()=> {unSubscribe()}
+        return () => { unSubscribe() }
     }, [])
 
 
 
-    const authInfo = { user, setUser, loading, setloading, createUser, loginUser,handleSignOut,jobs,setJobs }
+    const authInfo = { user, setUser, loading, setloading, createUser, loginUser, handleSignOut, jobs, setJobs }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
